d8fbd3d6aba34382807f0a48c1900d5bddec0282
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2024-03-06 09:17:25 -0500

    Update RealVsFakeOutlierDetector.py

--- RealVsFakeOutlierDetector.py
+++ RealVsFakeOutlierDetector.py
@@ -3,12 +3,15 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 
 class RealVsFakeOutlierDetector():
-    def __init__(self, max_depth=5):
+    def __init__(self, max_depth=None, max_leaf_nodes=None):
         self.clf = None
         self.max_depth = max_depth
+        self.max_leaf_nodes = max_leaf_nodes
 
     def fit(self, df, label_col):
-        self.clf = DecisionTreeClassifier(max_depth=self.max_depth, random_state=0) 
+        self.clf = DecisionTreeClassifier(max_depth=self.max_depth, 
+                                          max_leaf_nodes=self.max_leaf_nodes,
+                                          random_state=0) 
         self.clf.fit(df.drop(columns=['Real']), train_df['Real'])
     
     def predict(self, df):
