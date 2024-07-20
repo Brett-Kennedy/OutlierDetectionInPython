a1e9305377b4ff3aa242be09c80c81e0f8f92829
Brett-Kennedy <84815610+Brett-Kennedy@users.noreply.github.com>
2024-07-20 19:14:37 -0400

    Update RealVsFakeOutlierDetector.py

--- RealVsFakeOutlierDetector.py
+++ RealVsFakeOutlierDetector.py
@@ -3,16 +3,29 @@ import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 
 class RealVsFakeOutlierDetector():
-    def __init__(self, max_depth=None, max_leaf_nodes=None):
+    def __init__(self, max_depth=4, max_leaf_nodes=6):
         self.clf = None
         self.max_depth = max_depth
         self.max_leaf_nodes = max_leaf_nodes
 
-    def fit(self, df, label_col):
+    def fit(self, df):
+        real_df = df.copy()        
+        real_df['Real'] = True
+        
+        synth_df = pd.DataFrame() 
+        for col_name in data_df.columns:
+            mean = real_df[col_name].mean()
+            stddev = real_df[col_name].std()
+            synth_df[col_name] = np.random.normal(
+               loc=mean, scale=stddev, size=len(data_df))
+        synth_df['Real'] = False  
+        
+        full_df = pd.concat([real_df, synth_df])
+        
         self.clf = DecisionTreeClassifier(max_depth=self.max_depth, 
                                           max_leaf_nodes=self.max_leaf_nodes,
                                           random_state=0) 
-        self.clf.fit(df.drop(columns=[label_col]), train_df[label_col])
+        self.clf.fit(full_df.drop(columns=['Real']), full_df['Real'])
     
     def predict(self, df):
-        return self.clf.predict(df)
+        return self.clf.predict_proba(df)
